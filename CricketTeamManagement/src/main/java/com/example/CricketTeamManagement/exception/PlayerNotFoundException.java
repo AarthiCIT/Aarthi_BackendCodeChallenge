62f03584c0591dc7829195b7f15c82237f22104b
Aarthi <159801255+AarthiCIT@users.noreply.github.com>
2026-06-24 13:30:59 +0530

    Delete CricketTeamManagement directory

--- CricketTeamManagement/src/main/java/com/example/CricketTeamManagement/exception/PlayerNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.CricketTeamManagement.exception;
-
-public class PlayerNotFoundException extends RuntimeException {
-    public PlayerNotFoundException(Long id) {
-        super("Player not found with this id");
-    }
-}
--- CricketTeamManagement/src/main/java/com/example/CricketTeamManagement/repo/PlayerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.CricketTeamManagement.repo;
-
-import com.example.CricketTeamManagement.entity.Player;
-import com.example.CricketTeamManagement.exception.PlayerNotFoundException;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerRepository extends JpaRepository<Player, Long> {
-}
